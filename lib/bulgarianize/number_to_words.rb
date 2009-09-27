c774386a0f39a2ef6c4e0de5de5240271f1bf1c1
Stefan Kanev <stefan.kanev@gmail.com>
2009-09-27 15:14:30 +0300

    NumberToWords up to 999,999

--- lib/bulgarianize/number_to_words.rb
+++ lib/bulgarianize/number_to_words.rb
@@ -1,10 +1,21 @@
 module Bulgarianize
+  class Condition
+    def initialize(&block)
+      @block = block
+    end
+    
+    def ===(n)
+      @block[n]
+    end
+  end
+  
   module NumberToWords
     MALE_DIGITS = %w[нула един два три четири пет шест седем осем девет]
     FEMALE_DIGITS = %w[нула една две три четири пет шест седем осем девет]
     NEUTER_DIGITS = %w[нула едно две три четири пет шест седем осем девет]
     NAMES_11_TO_19 = %w[единадесет дванадесет тринадесет четиринадесет петданесет шестнадесет седемнадесет осемнадесет деветнадесет]
     MULTIPLES_OF_10 = %w[десет двадесет тридесет четиридесет петдесет шестдесет седемдесет осемдесет деветдесет]
+    HUNDREDS = %w[сто двеста триста четиристотин петстотин шестстотин седемстотин осемстотин деветстотин]
     
     def as_words(gender = :neuter)
       words_for(self.numeric, gender)
@@ -15,9 +26,35 @@ module Bulgarianize
         case n
           when 0..9 then {:male => MALE_DIGITS, :female => FEMALE_DIGITS, :neuter => NEUTER_DIGITS}.fetch(gender).fetch(n)
           when 11..19 then NAMES_11_TO_19.fetch(n - 11)
-          when 10, 20, 30, 40, 50, 60, 70, 80, 90 then MULTIPLES_OF_10.fetch(n / 10 - 1)
-          else  "#{words_for((n / 10) * 10, gender)} и #{words_for(n % 10, gender)}"
+          when cond { |n| n % 10 == 0 and n < 100 } then MULTIPLES_OF_10.fetch(n / 10 - 1)
+          when 21..99 then join_words words_for(whole_part(n, 10), gender), words_for(n % 10, gender)
+          when 100, 200, 300, 400, 500, 600, 700, 800, 900 then HUNDREDS.fetch(n / 100 - 1)
+          when 101..999 then join_words words_for(whole_part(n, 100), gender), words_for(n % 100, gender)
+          when 1000 then 'хиляда'
+          when cond { |n| n % 1_000 == 0 and n < 1_000_000 } then "#{words_for(n / 1000, :neuter)} хиляди"
+          when 1_001..999_999 then join_words words_for(whole_part(n, 1000), gender), words_for(n % 1000, gender)
+          when 1_000_000 then 'един милион'
         end
+      end
+      
+      def join_words(a, b)
+        if b.include? ' и '
+          "#{a} #{b}"
+        else
+          "#{a} и #{b}"
+        end
+      end
+      
+      def cond(&block)
+        Condition.new(&block)
+      end
+      
+      def whole_part(number, base)
+        (number / base) * base
+      end
+      
+      def reminder(number, base)
+        number % base
       end
   end
   
--- spec/number_to_words_spec.rb
+++ spec/number_to_words_spec.rb
@@ -34,9 +34,49 @@ describe "NumberToWords" do
     end
   end
   
-  it "should work with numbers less than 10, not dividable by 10" do
+  it "should work with numbers less than 100, not dividable by 10" do
     42.bg.as_words.should == 'четиридесет и две'
     97.bg.as_words.should == 'деветдесет и седем'
     89.bg.as_words.should == 'осемдесет и девет'
   end
+  
+  it "should work with numbers less than 1000, divisable by 100" do
+    %w[сто двеста триста четиристотин петстотин шестстотин седемстотин осемстотин деветстотин].zip([100, 200, 300, 400, 500, 600, 700, 800, 900]).each do |name, number|
+      number.bg.as_words.should == name
+    end
+  end
+  
+  it "should work with numbers 100-1000, not dividable by 100" do
+    110.bg.as_words.should == 'сто и десет'
+    217.bg.as_words.should == 'двеста и седемнадесет'
+    420.bg.as_words.should == 'четиристотин и двадесет'
+    
+    123.bg.as_words.should == 'сто двадесет и три'
+  end
+  
+  it "should work with numbers divisable with 1000" do
+    1_000.bg.as_words.should == 'хиляда'
+    2_000.bg.as_words.should == 'две хиляди'
+    5_000.bg.as_words.should == 'пет хиляди'
+    10_000.bg.as_words.should == 'десет хиляди'
+    13_000.bg.as_words.should == 'тринадесет хиляди'
+    28_000.bg.as_words.should == 'двадесет и осем хиляди'
+    100_000.bg.as_words.should == 'сто хиляди'
+    673_000.bg.as_words.should == 'шестстотин седемдесет и три хиляди'
+  end
+  
+  it "should work with numbers less than 1 000 000, not divisable with 1000" do
+    1_001.bg.as_words.should == 'хиляда и едно'
+    1_024.bg.as_words.should == 'хиляда двадесет и четири'
+    17_001.bg.as_words.should == 'седемнадесет хиляди и едно'
+    24_200.bg.as_words.should == 'двадесет и четири хиляди и двеста'
+    65_536.bg.as_words.should == 'шестдесет и пет хиляди петстотин тридесет и шест'
+    
+    123_456.bg.as_words.should == 'сто двадесет и три хиляди четиристотин петдесет и шест'
+  end
+  
+  it "should work with millions" do
+    1_000_000.bg.as_words.should == 'един милион'
+  end
+  
 end
